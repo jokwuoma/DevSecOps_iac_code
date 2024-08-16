8feeafceb7dd348112824b8680930462dc9b19bc
jokwuoma <jy.noka@gmail.com>
2024-08-15 23:52:56 -0700

    Initial commit.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
     }
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "devsecops-quiz-app" // changed bucket name from cloudcore007 to devsecops-quiz-app
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2" // changed region from us-east-1 to us-west-2
   }
 
   required_version = ">= 1.6.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,50 +1,50 @@
 variable "region" {
   description = "AWS region"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-west-2" // changed from us-east-1 to us-west-2
 }
 
 variable "vpc-name" {
   description = "VPC Name for our Jumphost server"
-  type = string
-  default = "Jumphost-vpc"
+  type        = string
+  default     = "Jumphost-vpc"
 }
 
 variable "igw-name" {
   description = "Internet Gate Way Name for our Jumphost server"
-  type = string
-  default = "Jumphost-igw"
+  type        = string
+  default     = "Jumphost-igw"
 }
 
 variable "subnet-name" {
   description = "Subnet Name for our Jumphost server"
-  type = string
-  default = "Jumphost-subnet"
+  type        = string
+  default     = "Jumphost-subnet"
 }
 
 variable "rt-name" {
   description = "Route Table Name for our Jumphost server"
-  type = string
-  default = "Jumphost-rt"
+  type        = string
+  default     = "Jumphost-rt"
 }
 
 variable "sg-name" {
   description = "Security Group for our Jumphost server"
-  type = string
-  default = "Jumphost-sg"
+  type        = string
+  default     = "Jumphost-sg"
 }
 
 
 variable "iam-role" {
   description = "IAM Role for the Jumphost Server"
-  type = string
-  default = "Jumphost-iam-role"
+  type        = string
+  default     = "Jumphost-iam-role"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-0aff18ec83b712f05" // Replaced with the latest AMI ID Ubuntu; initial AMI ID was ami-0c7217cdde317cfec
 }
 
 variable "instance_type" {
@@ -56,7 +56,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "DevSecOps_Key" // changed name of EC2 key pair from gitopskey to DevSecOps_Key
 }
 
 variable "instance_name" {
